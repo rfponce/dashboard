0cb520b022959e7e293ea4c8cf30ae02789fdd9d
Fabián Ponce <maximohumano@gmail.com>
2023-06-18 19:08:29 -0600

    Add attributions window

---
+++ script.js
@@ -0,0 +1,9 @@
+let attributions_window = document.getElementById("attributions");
+
+function show_window() {
+    attributions_window.style.display = "flex";
+}
+
+function hide_window() {
+    attributions_window.style.display = "none";
+}
